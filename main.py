aa3d5e97b72ced7d7157fb5d2e6aac66c491ea2a
Kartik <kartikvaghasiya10@gmail.com>
2024-10-08 14:16:57 +0530

    age added in kartik

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 
 def greet(name):
     print(f"Hello, {name}!")
+    
+def age(age):
+    print(f"You're {age}")
 
 def main():
     greet("Kartik")
+    age("20")
 
 if __name__ == "__main__":
     main()
